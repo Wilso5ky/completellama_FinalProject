38cd761af9cb65065ce7c73b7426f0246ccd345c
tipkemam <tipkemam@LCBLAB2230-22>
2024-12-09 16:28:27 -0500

    requirements documentation

--- completellama_FinalProject/mainPackage/main.py
+++ completellama_FinalProject/mainPackage/main.py
@@ -1,3 +1,17 @@
+# Name: Alexis Tipkemper-Sparks / Kayla Wilson / Jared Rababy 
+# email:  tipkemam@mail.uc.edu / wilso5ky@mail.uc.edu /rababyjd@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date: 12/10/2024
+# Course #/Section:  IS 4010
+# Semester/Year: Fall 2024
+# Brief Description of the assignment:  Decrypting codes from JSON files + scavenging 
+
+# Brief Description of what this module does: Practice reading JSON files, Python, and outputting photos
+# Citations: GeeksforGeeks / W3Schools / InClass from last week
+# Anything else that's relevant: N/A Github makes us sad
+
+#main.py
+
 import dataPackage
 from locationPackage.decryptedLocation import findLocation
 from moviePackage.decryptedMovie import decrypt_movie_title
